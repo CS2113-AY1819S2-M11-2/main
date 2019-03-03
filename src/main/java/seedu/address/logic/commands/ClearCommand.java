691ca01fecbb5cd94462532fccea8260b41d0487
khairulhazril <khairulhazril@gmail.com>
2019-03-03 19:28:52 +0800

    Edit some texts to display Task Manager instead of Address Book

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -5,6 +5,7 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.model.TaskManager;
 
 /**
  * Clears the address book.
@@ -12,13 +13,13 @@ import seedu.address.model.Model;
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_SUCCESS = "Task Manager has been cleared!";
 
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
-        model.setAddressBook(new AddressBook());
+        model.setTaskManager(new TaskManager());
         model.commitAddressBook();
         return new CommandResult(MESSAGE_SUCCESS);
     }
